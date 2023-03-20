bc713d50bdcc46da16c799861a7e979aef5815dc
Henno Täht <henno.taht@gmail.com>
2023-03-20 10:18:06 +0200

    As a user, I want to be able to sign in and out of the virtual clinic app, so that I can access my consultations securely and prevent unauthorized access [Delivers #185343944]

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yamlJs = require('yamljs');
 const swaggerDocument = yamlJs.load('./swagger.yaml');
+const {v4: uuidv4} = require('uuid');
 
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
+let sessions = [];
+let users = [
+    {id: 1, email: 'admin@admin.com', password: 'admin'}
+];
 // Serve static files
 app.use(express.static('public'));
 
@@ -22,6 +27,93 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     const status = err.statusCode || 500;
     res.status(status).send(err.message);
+})
+
+// Routes
+app.post('/sessions', (req, res) => {
+    // Validate email and password
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).send('Email and password are required')
+    }
+    // Get user
+    const user = users.find(user => user.email === req.body.email)
+    if (!user) {
+        return res.status(400).send('User not found')
+    }
+    // Check password
+    if (user.password !== req.body.password) {
+        return res.status(400).send('Invalid password')
+    }
+
+    // If valid, create a session
+    const session = {
+        id: uuidv4(),
+        userId: user.id,
+        createdAt: new Date()
+    }
+
+    // Add session to sessions array
+    sessions.push(session)
+
+    // Return a new session
+    res.status(201).send({sessionId: session.id})
+})
+
+function authenticateRequest(req, res, next) {
+
+    // Check for authorization header
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send('Authorization header is required')
+    }
+
+    // Check Authorization header format
+    const authHeaderParts = authHeader.split(' ')
+    if (authHeaderParts.length !== 2 || authHeaderParts[0] !== 'Bearer') {
+        return res.status(401).send('Authorization header format must be "Bearer {token}"')
+    }
+
+    // Get session ID from header
+    const sessionId = authHeaderParts[1]
+
+    // Validate session ID
+    if (!sessionId) {
+        return res.status(401).send('Session ID is required')
+    }
+
+    // Get session
+    const session = sessions.find(session => session.id === sessionId)
+
+    // If session not found, return 401
+    if (!session) {
+        return res.status(401).send('Invalid session')
+    }
+
+    // Get user
+    const user = users.find(user => user.id === session.userId)
+
+    // Validate user
+    if (!user) {
+        return res.status(401).send('User not found')
+    }
+
+    // Add session to request
+    req.session = session
+
+    // Add user to request
+    req.user = user
+
+    // Continue processing the request
+    next()
+}
+
+app.delete('/sessions', authenticateRequest, (req, res) => {
+
+    // Remove session from sessions array
+    sessions = sessions.filter(session => session.id !== req.session.id)
+
+    // Return a 204 with no content if the session was deleted
+    res.status(204).send()
 })
 
 app.listen(port, () => {
